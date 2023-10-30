d03240038fcc4bdf0787efe377c9eecc8418dbec
snil <silna5537@gmail.com>
2023-10-30 13:05:47 +0900

    [level unrated] Title: 문자열 출력하기, Time: 136.14 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+    
+        System.out.println(str);
+        
+        
+    }
+    
+}
