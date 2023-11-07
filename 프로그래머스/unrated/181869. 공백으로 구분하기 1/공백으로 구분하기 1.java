cefb8198530d2d44a9f4102a4cee93361a0f4c0b
snil <silna5537@gmail.com>
2023-11-07 18:06:27 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {my_string};
+        
+        String[] a = my_string.split(" ");
+        
+        return a;
+         
+    }
+}
